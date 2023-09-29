fe98ee28f0a3f329a2953c47299a8c71b3ace9ac
dwilliams304 <d.williams0434@gmail.com>
2023-09-29 11:13:13 -0700

    Router and middleware wired

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,66 @@
 const router = require('express').Router()
+const Accounts = require('./accounts-model');
+const middleware = require('./accounts-middleware');
 
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
   // DO YOUR MAGIC
+  try{
+      
+  }
+  catch(err){
+    next(err)
+  }
 })
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id', middleware.checkAccountId, async (req, res, next) => {
   // DO YOUR MAGIC
+  try{
+    
+  }
+  catch(err){
+    next(err)
+  }
 })
 
-router.post('/', (req, res, next) => {
+router.post('/', middleware.checkAccountPayload, 
+middleware.checkAccountNameUnique, async (req, res, next) => {
   // DO YOUR MAGIC
+  try{
+    
+  }
+  catch(err){
+    next(err)
+  }
 })
 
-router.put('/:id', (req, res, next) => {
+router.put('/:id', middleware.checkAccountId, middleware.checkAccountPayload, 
+middleware.checkAccountNameUnique, async (req, res, next) => {
   // DO YOUR MAGIC
+  try{
+    
+  }
+  catch(err){
+    next(err)
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', middleware.checkAccountId, async (req, res, next) => {
   // DO YOUR MAGIC
+  try{
+    
+  }
+  catch(err){
+    next(err)
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
   // DO YOUR MAGIC
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack
+  })
+ 
 })
 
 module.exports = router;
